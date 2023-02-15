7c92b5e6c4cd3f34167f88674135734934156e06
Inaki Villar <inaki.seri@gmail.com>
2023-02-15 11:33:05 -0800

    removing custom user data plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,7 +26,7 @@ pluginManagement {
 }
 
 plugins {
-    id("com.gradle.common-custom-user-data-gradle-plugin") version "1.8.1"
+  //  id("com.gradle.common-custom-user-data-gradle-plugin") version "1.8.1"
     id("com.gradle.enterprise") version "3.11.1"
     id("androidx.build.gradle.gcpbuildcache") version "1.0.0-beta01"
 
