b4a8dd4ff73653a2914ae306ed271297ea4be23a
Inaki Villar <inaki.seri@gmail.com>
2023-02-15 11:21:44 -0800

    a

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,95 +36,95 @@ plugins {
     id("io.github.cdsap.testprocess") version "0.1"
 }
 
-
-allprojects {
-    tasks.withType<Test>().configureEach {
-        maxParallelForks = System.getenv("MAX_PARALLEL_FORKS").toInt()
-    }
-
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-
-
-        doLast {
-
-            rootProject.buildScan.value(
-                "${this@configureEach.path}-freeCompilerArgs",
-                "${this@configureEach.compilerOptions.freeCompilerArgs.get().joinToString(",")}"
-            )
-
-            rootProject.buildScan.value(
-                "${this@configureEach.path}-compilerOptions",
-                "${this@configureEach.compilerOptions.toString()}"
-            )
-
-
-
-
-            var x = ""
-            this@configureEach.classpathSnapshotProperties.classpath.forEach {
-                x += it.path+ ";"
-            }
-            rootProject.buildScan.value(
-            "${this@configureEach.path}-kotlinJavaToolchainProvider",
-            "${this@configureEach.kotlinJavaToolchainProvider.get().javaVersion.get()}"
-            )
-            rootProject.buildScan.value(
-                "${this@configureEach.path}-isInfoAsWarnings",
-                "${this@configureEach.kotlinJavaToolchainProvider.get().toolchain.toString()}"
-            )
-            rootProject.buildScan.value(
-                "${this@configureEach.path}-usePreciseJavaTracking",
-                "${this@configureEach.usePreciseJavaTracking}"
-            )
-            rootProject.buildScan.value(
-                "${this@configureEach.path}-usePreciseclasspathSnapshotPropertiesuseClasspathSnapshot",
-                "${this@configureEach.classpathSnapshotProperties.useClasspathSnapshot.get()}"
-            )
-            rootProject.buildScan.value(
-                "${this@configureEach.path}-serializedCompilerArguments",
-                "${this@configureEach.serializedCompilerArguments.toString()}"
-            )
-
-            rootProject.buildScan.value(
-                "${this@configureEach.path}-classpath",
-                "${x}"
-            )
-
-            rootProject.buildScan.value(
-                "${this@configureEach.path}-size",
-                "${this@configureEach.pluginOptions.get().size}"
-            )
-
-            rootProject.buildScan.value(
-                "${this@configureEach.path}-size",
-                "${this@configureEach.pluginOptions.get().size}"
-            )
-            this@configureEach.pluginOptions.get().forEach {
-
-                println(it.getAsTaskInputArgs().size)
-                println(it.allOptions().size)
-                rootProject.buildScan.value(
-                    "${this@configureEach.path}-inputs",
-                    "${it.toString()}"
-                )
-                rootProject.buildScan.value(
-                    "${this@configureEach.path}-inputs-allOptions",
-                    "${it.allOptions().toString()}"
-                )
-                rootProject.buildScan.value(
-                    "${this@configureEach.path}-inputs-getAsTaskInputArgs",
-                    "${it.getAsTaskInputArgs().toString()}"
-                )
-
-                it.allOptions().forEach{
-                    rootProject.buildScan.value(
-                        "${this@configureEach.path}",
-                        it.value.joinToString(",")
-                    )
-                }
-            }
-
-
-        }
-    }
-}
+//
+//allprojects {
+//    tasks.withType<Test>().configureEach {
+//        maxParallelForks = System.getenv("MAX_PARALLEL_FORKS").toInt()
+//    }
+//
+//    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+//
+//
+//        doLast {
+//
+//            rootProject.buildScan.value(
+//                "${this@configureEach.path}-freeCompilerArgs",
+//                "${this@configureEach.compilerOptions.freeCompilerArgs.get().joinToString(",")}"
+//            )
+//
+//            rootProject.buildScan.value(
+//                "${this@configureEach.path}-compilerOptions",
+//                "${this@configureEach.compilerOptions.toString()}"
+//            )
+//
+//
+//
+//
+//            var x = ""
+//            this@configureEach.classpathSnapshotProperties.classpath.forEach {
+//                x += it.path+ ";"
+//            }
+//            rootProject.buildScan.value(
+//            "${this@configureEach.path}-kotlinJavaToolchainProvider",
+//            "${this@configureEach.kotlinJavaToolchainProvider.get().javaVersion.get()}"
+//            )
+//            rootProject.buildScan.value(
+//                "${this@configureEach.path}-isInfoAsWarnings",
+//                "${this@configureEach.kotlinJavaToolchainProvider.get().toolchain.toString()}"
+//            )
+//            rootProject.buildScan.value(
+//                "${this@configureEach.path}-usePreciseJavaTracking",
+//                "${this@configureEach.usePreciseJavaTracking}"
+//            )
+//            rootProject.buildScan.value(
+//                "${this@configureEach.path}-usePreciseclasspathSnapshotPropertiesuseClasspathSnapshot",
+//                "${this@configureEach.classpathSnapshotProperties.useClasspathSnapshot.get()}"
+//            )
+//            rootProject.buildScan.value(
+//                "${this@configureEach.path}-serializedCompilerArguments",
+//                "${this@configureEach.serializedCompilerArguments.toString()}"
+//            )
+//
+//            rootProject.buildScan.value(
+//                "${this@configureEach.path}-classpath",
+//                "${x}"
+//            )
+//
+//            rootProject.buildScan.value(
+//                "${this@configureEach.path}-size",
+//                "${this@configureEach.pluginOptions.get().size}"
+//            )
+//
+//            rootProject.buildScan.value(
+//                "${this@configureEach.path}-size",
+//                "${this@configureEach.pluginOptions.get().size}"
+//            )
+//            this@configureEach.pluginOptions.get().forEach {
+//
+//                println(it.getAsTaskInputArgs().size)
+//                println(it.allOptions().size)
+//                rootProject.buildScan.value(
+//                    "${this@configureEach.path}-inputs",
+//                    "${it.toString()}"
+//                )
+//                rootProject.buildScan.value(
+//                    "${this@configureEach.path}-inputs-allOptions",
+//                    "${it.allOptions().toString()}"
+//                )
+//                rootProject.buildScan.value(
+//                    "${this@configureEach.path}-inputs-getAsTaskInputArgs",
+//                    "${it.getAsTaskInputArgs().toString()}"
+//                )
+//
+//                it.allOptions().forEach{
+//                    rootProject.buildScan.value(
+//                        "${this@configureEach.path}",
+//                        it.value.joinToString(",")
+//                    )
+//                }
+//            }
+//
+//
+//        }
+//    }
+//}
