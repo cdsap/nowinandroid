d04bf62a23c129b430dee0f9c9fbee7b86d74777
Inaki Villar <inaki.seri@gmail.com>
2023-10-16 13:54:51 -0700

    updating repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -30,6 +30,23 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+plugins {
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "1.8.1"
+    id("com.gradle.enterprise") version "3.15.1"
+}
+
+gradleEnterprise {
+    server = "http://ge.solutions-team.gradle.com"
+    allowUntrustedServer = true
+    buildScan {
+        publishAlways()
+        capture {
+            isTaskInputFiles = true
+        }
+        isUploadInBackground = System.getenv("CI") == null
+    }
+}
+
 rootProject.name = "nowinandroid"
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
